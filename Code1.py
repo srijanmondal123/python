3bb048e0346885a55b3638ed24e5fdd3c204e2e7
srijanmondal123 <msrijan959@gmail.com>
2025-12-27 17:31:49 +0530

    newcode

---
+++ Code1.py
@@ -0,0 +1,4 @@
+a=int(input("Enter 1st no: "))
+b=int(input("Enter 2nd no: "))
+c=a+b
+print(a,"+",b,"=",c)
---
+++ Code2.py
@@ -0,0 +1,5 @@
+age=int(input("Please enter your age: "))
+if(age>17):
+    print("Voter")
+else:
+    print("Non-Voter")
